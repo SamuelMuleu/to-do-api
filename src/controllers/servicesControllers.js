64198e208efd9cb95c89174395e56c04b02bd504
samuel pereira <samuelpereiramuleu@gmail.com>
2025-01-22 21:28:22 -0300

    connect to mongodb using mongoose

--- src/controllers/servicesControllers.js
+++ src/controllers/servicesControllers.js
@@ -1,40 +1,53 @@
 const { v4: uuidv4 } = require("uuid");
 
-const todos = [];
+const mongoose = require("mongoose");
 
-const CreateServices = (req, res) => {
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://samuelmuleu:Aurora2@cluster0.7fvba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  );
+}
+main().catch((err) => console.log(err));
+const Taks = mongoose.model("task", {
+  title: String,
+  completed: Boolean,
+});
+
+const CreateServices = async (req, res) => {
   const { title, completed } = req.body;
 
   if (!title) {
     return res.status(400).json({ error: "O título da tarefa é obrigatório!" });
   }
 
-  const newTodo = {
+  const newTask = new Taks({
     id: uuidv4(),
     title: title,
-    completed: completed || false,
-  };
+    completed: completed,
+  });
 
-  todos.push(newTodo);
+  await newTask.save();
+  res.send(newTask);
 
-  return res.status(201).json(newTodo);
+  return res.status(201).json(newTask);
 };
 
-const getServices = (req, res) => {
-  return res.json(todos);
+const getServices = async(req, res) => {
+  const tasks = await Taks.find()
+   return  res.json(tasks);
+   
 };
 
 const deleteServices = (req, res) => {
   const { id } = req.params;
 
-  const services = todos;
-  const servicesIndex = services.findIndex(service => service.id === id );
-  if(servicesIndex === -1){
-    return res.status(404).json({ message: 'Produto não encontrado' });
+  const services = Taks;
+  const servicesIndex = services.findIndex((service) => service.id === id);
+  if (servicesIndex === -1) {
+    return res.status(404).json({ message: "Produto não encontrado" });
   }
-  services.splice(servicesIndex,1);
-  res.status(200).json({ message: 'Produto excluído com sucesso' });
-
+  services.splice(servicesIndex, 1);
+  res.status(200).json({ message: "Produto excluído com sucesso" });
 };
 
-module.exports = { CreateServices, getServices,deleteServices };
+module.exports = { CreateServices, getServices, deleteServices };
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const servicesRoutes = require('./routes/services')
+
+
 const app = express();
 const port = 3000;
 
 app.use(express.json())
-
-app.use('/todos',servicesRoutes)
+app.use('/tasks',servicesRoutes)
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
