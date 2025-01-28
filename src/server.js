4d0b247d4c6ceb700e0db72d0c327cfecfab8ead
samuel pereira <samuelpereiramuleu@gmail.com>
2025-01-28 19:38:27 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const cors = require('cors');
 const app = express();
 const port = 3001
 
+app.use(cors());
 app.use(express.json())
 app.use('/tasks',servicesRoutes)
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
